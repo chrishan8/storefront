6c62bc7e895fa461e1130442759619760c7b4023
Chris Han <chris.han8@gmail.com>
2019-05-10 01:13:09 -0400

    Add carousel component * Created carousel component using Swiper * Created footer component

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,47 @@
 <template>
-  <h1>Hello World</h1>
+  <div v-swiper:mySwiper="swiperOption">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide" v-for="(s, index) of slides" :key="index">
+        <img :src="s.img" />
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
+      slides: [
+        {
+          img: 'https://via.placeholder.com/1600x480',
+          text: '1'
+        }, {
+          img: 'https://via.placeholder.com/1600x480',
+          text: '2'
+        }, {
+          img: 'https://via.placeholder.com/1600x480',
+          text: '3'
+        }, {
+          img: 'https://via.placeholder.com/1600x480',
+          text: '4'
+        }
+      ],
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      }
+    }
+  }
 }
 </script>
 
 <style>
+  .swiper-slide img {
+    object-fit: cover;
+    width: 100%;
+  }
 </style>
